1996ba870c851fc595e661bd02ff683c4d0ba7bf
tom <tom@Rhovanion>
2016-08-23 16:56:39 +0200

    MakePDF class: - Added method to make new file - Added method to close file

--- src/mmb/thjkral/primertopdf/model/MakePDF.java
+++ src/mmb/thjkral/primertopdf/model/MakePDF.java
@@ -8,11 +8,36 @@
  */
 package mmb.thjkral.primertopdf.model;
 
+import com.itextpdf.text.*;
+
 /**
  * Creates the PDF file.
  * The file contains the best primers that Primer3 reported
  * @author tom
  */
 public class MakePDF {
+    
+    /**
+     * Makes the document.
+     * Adds some meta-data too
+     * @return 
+     */
+    public Document makeDocument () {
+        
+        Document doc = new Document();
+        doc.addTitle("Primers - Galaxy output");
+        doc.addSubject("Valiable primers");
+        doc.addAuthor("Tom Kral (MMB)");
+        doc.addCreator("Tom Kral (MMB)");
+        doc.addCreationDate();
+        
+        return doc;        
+    }
+    
+    public void close (Document doc) {
+        doc.close();
+    }
+    
+    
     
 }
