9fe393a4f142ccb472d9e948309b1f7967d67254
tom <tom@Rhovanion>
2016-08-23 12:24:42 +0200

    New Classes: - PrimerPair - Template
    CLI Class is finished

--- src/mmb/thjkral/primertopdf/model/PrimerPair.java
+++ src/mmb/thjkral/primertopdf/model/PrimerPair.java
@@ -12,6 +12,19 @@ package mmb.thjkral.primertopdf.model;
  * 
  * @author tom
  */
-public class PrimerPair {
+public class PrimerPair extends Template{
+    
+    int pair_no;
+    private String sequenceForward;
+    private String sequenceReverse;
+    private double GCcontent_f;
+    private double GCcontent_r;
+    private double MeltingTemp_f;
+    private double MeltingTemp_r;
+    private int productSize;
+
+    
+    
+    
     
 }
