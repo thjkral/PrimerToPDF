51f6feb22a9e1dbd0e33f8dab403b05d30381d96
tom <tom@Rhovanion>
2016-08-26 15:07:20 +0200

    PDF is made, all needed info present

---
+++ src/mmb/thjkral/primertopdf/model/RoundAllDoubles.java
@@ -0,0 +1,30 @@
+/*
+ * This code is created by:
+ * Tom Kral | Bioinformatics intern
+ * contact@tomkral.nl | +31642935544 | tomkral.nl
+ */
+package mmb.thjkral.primertopdf.model;
+
+import java.util.ArrayList;
+
+/**
+ * Rounds all Doubles in a list.
+ * 
+ * @author tom
+ */
+public class RoundAllDoubles {
+    
+    
+    /**
+     * Does the rounding.
+     * To two decimals.
+     * 
+     * @param list
+     */
+    public void round (ArrayList<Header> list) {
+        
+        
+        
+    }
+    
+}
